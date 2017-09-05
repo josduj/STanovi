0e20efdecf196577b4ffaae266608c82735e5bbb
josduj <josduj@users.noreply.github.com>
2017-09-06 01:24:48 +0200

    translate

--- bin/watcher-cli.js
+++ bin/watcher-cli.js
@@ -2,15 +2,14 @@
 
 const argv = require('yargs')
 	.usage('Usage: $0 <recipients> [options]')
-	.option('location', {alias: 'l', describe: 'id lokacije'})
-	.option('interval', {alias: 'i', describe: 'interval osvježavanja (min)'})
-	.option('min', {alias: 'm', describe: 'najniža cijena (€)'})
-	.option('max', {alias: 'M', describe: 'najviša cijena (€)'})
-	.option('u', {boolean: true, default: undefined, describe: 'only user submitted ads'})
+	.option('location', {alias: 'l', describe: 'location id'})
+	.option('interval', {alias: 'i', describe: 'refresh interval (min)'})
+	.option('min', {alias: 'm', describe: 'lowest price (€)'})
+	.option('max', {alias: 'M', describe: 'highest price (€)'})
+	.option('u', {boolean: true, default: undefined, describe: 'only user submitted ads (no 3rd party)'})
 	.option('p', {boolean: true, default: undefined, describe: 'only ads with pictures'})
 	.help('h')
 	.alias('h', 'help')
-	.example('Svakih 10 minuta na navedene mailove pošalji nove oglase sa lokacijom u Splitu, cijenom od 0 do 400 eura, koji nisu preko agencije, neovisno imaju li sliku')
 	.example('$ $0 foo@bar.com foo@foo.com -m 0 -M 400 -i 10 -l 1578 -u --no-p')
 	.argv
 
@@ -31,29 +30,29 @@ async function locationSelector (location) {
 	if (location !== undefined)
 		return location
 
-	let selected = util.selectFromArray(counties, 'Odaberi zupaniju')
+	let selected = util.selectFromArray(counties, 'Select county')
 	if (selected == 0)
 		return selected
 
 	const cities = await api.getCities(selected)
-	let temp = util.selectFromArray(cities, 'Odaberi grad')
+	let temp = util.selectFromArray(cities, 'Select city')
 	if (temp == selected)
 		return selected
 	else
 		selected = temp
 
 	const areas = await api.getCities(selected)
-	return util.selectFromArray(areas, 'Odaberi naselje')
+	return util.selectFromArray(areas, 'Select area')
 }
 
 async function main () { 
-	const interval = util.inputInt('Interval osvjezavanja (min): ', argv.interval)
+	const interval = util.inputInt('Refresh interval (min): ', argv.interval)
 	const query = { price : {} }
 	query.locationId = await locationSelector(argv.location)
-	query.price.min = util.inputInt('Min cijena (€): ', argv.min)
-	query.price.max = util.inputInt('Max cijena (€): ', argv.max)
-	query.userOnly = util.selectBoolean('Bez agencija? ', argv.u)
-	query.adsWithImages = util.selectBoolean('Samo sa slikom? ', argv.p)
+	query.price.min = util.inputInt('Min price (€): ', argv.min)
+	query.price.max = util.inputInt('Max price (€): ', argv.max)
+	query.userOnly = util.selectBoolean('Only user submitted ads? ', argv.u)
+	query.adsWithImages = util.selectBoolean('Only ads with pictures? ', argv.p)
 	query.date = new Date()
 	util.print()
 
